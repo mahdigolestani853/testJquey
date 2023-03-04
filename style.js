53b4dd38b83130d2fc4fd867303671cb7e828534
mahdigolestani853 <mahdigolestani853@yaoo.com>
2023-03-04 14:18:50 +0330

    updare style.js

--- style.js
+++ style.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       div.animate({height: '100px', opacity: '0.4'}, "slow");
       div.animate({width: '100px', opacity: '0.8'}, "slow",function(){
         if(div.attr('d-name')=='custom'){
-          alert('ok');
+          alert('no');
         }
       });
     })
